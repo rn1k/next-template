c8a963c05c57daa5dd0bd7ac8f8e8fd4811365f5
rn1k <maki_r12@yahoo.co.jp>
2019-02-10 02:33:29 +0900

    storybookを追加

---
+++ stories/sample/index.stories.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import styled from 'styled-components';
+
+const StyledCaption = styled.h1`
+    margin: 0 0 12px 0;
+    color: black;
+    font-size: 2rem;
+    font-weight: bold;
+    line-height: 1.4;
+    background-color: #cccccc;
+`;
+
+storiesOf('Button', module)
+    .add('with text', () => <StyledCaption>Hello Button</StyledCaption>)
+    .add('with some emoji', () => (
+        <StyledCaption>
+            <span role="img" aria-label="so cool">
+                😀 😎 👍 💯
+            </span>
+        </StyledCaption>
+    ));
