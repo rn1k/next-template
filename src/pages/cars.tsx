bf2a288e16c9a8c1e29d19c0c131aa894c761e08
rn1k <maki_r12@yahoo.co.jp>
2019-02-23 11:58:59 +0900

    デフォルトのサンプル削除

--- src/pages/cars.tsx
+++
@@ -1,5 +0,0 @@
-import CarsOverview from '../modules/cars/Overview';
-
-const CarsPage = () => <CarsOverview />;
-
-export default CarsPage;
--- src/pages/login.tsx
+++
@@ -1,5 +0,0 @@
-import Login from '../modules/auth/Login';
-
-const LoginPage = () => <Login />;
-
-export default LoginPage;
